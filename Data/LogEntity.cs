369b950a554123105ad609b64e95b1d20faf42d1
nkamaras <nikos.7kamaras@gmail.com>
2023-12-04 16:29:50 +0200

    Add files via upload

---
+++ Data/LogEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureLoggingFunction.Data
+{
+    public class LogEntity
+    {
+        public int severity { get; set; }
+        public string LogMessage { get; set; }
+        public string module { get; set; }
+    }
+}
